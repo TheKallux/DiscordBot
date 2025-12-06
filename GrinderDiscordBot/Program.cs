859c35f6be19aaef48f2167955be5bedd91a3c40
TheKallux <kallelindberg.jobb@gmail.com>
2025-12-06 16:15:20 +0100

    Add project files.

---
+++ GrinderDiscordBot/Program.cs
@@ -0,0 +1,27 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+public class program
+{
+
+    private static Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+
+    private static DiscordSocketClient _client;
+
+    public static async Task Main()
+    {
+        _client = new DiscordSocketClient();
+
+        _client.Log += Log;
+
+        var token = File.ReadAllText("");
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+        await Task.Delay(-1);
+    }
+}
